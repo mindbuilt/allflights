a9e04153444b70eb502ae1e5d9682c123b1a4dfe
Oliver J Moran <oliverjmoran@Olivers-MacBook-Pro-2.local>
2025-07-03 12:13:06 +1000

    Move serverless function to /api for Vercel

---
+++ api/flights.js
@@ -0,0 +1,36 @@
+const flights = [
+  {
+    flight: "QF123",
+    airline: "Qantas",
+    from: "SYD",
+    to: "MEL",
+    time: "08:30",
+    status: "On Time",
+  },
+  {
+    flight: "VA456",
+    airline: "Virgin",
+    from: "MEL",
+    to: "BNE",
+    time: "09:15",
+    status: "Delayed",
+  },
+  {
+    flight: "JQ789",
+    airline: "Jetstar",
+    from: "BNE",
+    to: "CNS",
+    time: "10:00",
+    status: "Boarding",
+  },
+  {
+    flight: "NZ321",
+    airline: "Air NZ",
+    from: "SYD",
+    to: "AKL",
+    time: "11:20",
+    status: "Departed",
+  },
+];
+
+export default flights;
